f5bea5c5a0f7c2ae9b86591133121e9c3d05dc58
dm-creations <d.xl.mccormack@gmail.com>
2023-07-08 16:29:06 +0100

    Mostly implemented (need snake sprite texture)

--- src/Snake.ts
+++ src/Snake.ts
@@ -1,78 +1,336 @@
 import * as Phaser from 'phaser';
-export default class Snake extends Phaser.Physics.Arcade.Group {
+// import Demo from './game';
+export default class Snake {
 
-    private headPosition: Phaser.Geom.Point;
-    // private body: what type should this be?
-    public body: Phaser.Physics.Arcade.Body;
-    public head: Phaser.GameObjects.Sprite;
-    public alive: boolean;
-    private speed: number;
-    private moveTime: number;
-    private heading: number;
-    private direction: number;
+    // this might need exporting
+    private gameContainer: HTMLElement = document.querySelector('#game') || document.body
+    private gameContainerBCR = this.gameContainer.getBoundingClientRect()
+    private  gameContainerW = this.gameContainerBCR.width // window.innerWidth
+    private  gameContainerH = this.gameContainerBCR.height
+    private ceilsXCount = this.getCeilsCountByPixelsCount(this.gameContainerW) // - 1 (if scroll)
+    private ceilsYCount = this.getCeilsCountByPixelsCount(this.gameContainerH)
+    private headPosition: Phaser.Geom.Point
+    public snakeHeadX: number
+    public snakeHeadY: number
+    public bodyPartsSprites: Phaser.GameObjects.Sprite[] = []
+    public bodyPartsPositions: any[]
+    public bodyPartsBonuses: any[] = []
+    public snakeLength: number // size
+    public readonly DIRECTIONS = {
+        UP: 'up',
+        RIGHT: 'right',
+        DOWN: 'down',
+        LEFT: 'left'
+    }
+    public lives: number
+    private initialLength: number = 20
+    public alive: boolean = true
+    private currentDir: string = this.DIRECTIONS.UP
+    private lastMoveDir: string = this.currentDir
+    private scene: Phaser.Scene
+    private ceil: number
 
-    private UP = 0;
-    private DOWN = 1;
-    private LEFT = 2;
-    private RIGHT = 3;
+    
 
     // initialize the snake
     constructor(scene, x, y) {
-        super(scene, 'snake');
-        this.headPosition = new Phaser.Geom.Point(x, y);
+        this.scene = scene
+        this.snakeHeadX = x
+        this.snakeHeadY = y
+        this.ceil = 32
+        this.snakeLength = 32 // this.scene.ceil * this.snakeCeilsSize
+        this.bodyPartsPositions = this.getInitialSnakeBody()
 
-        this.body = scene.add.group(); // group is 
-
-        this.head = this.body.create(x * -16, y * 16, 'body');
-        this.head.setOrigin(0);
-
-        this.alive = true;
-
-        this.speed = 100;
-
-        this.moveTime = 0;
-
-        this.heading = this.RIGHT;
-        this.direction = this.RIGHT;
-        console.log(this.headPosition);
+        // build snake
+        this.buildSnake()
     }
-    move (time)
-        {
-            /**
-            * Based on the heading property (which is the direction the pgroup pressed)
-            * we update the headPosition value accordingly.
-            * 
-            * The Math.wrap call allow the snake to wrap around the screen, so when
-            * it goes off any of the sides it re-appears on the other.
-            */
-            switch (this.heading)
-            {
-                case this.LEFT:
-                    this.headPosition.x = Phaser.Math.Wrap(this.headPosition.x - 1, 0, 40); // this position should change based on the device
-                    break;
+    public getCeilsCountByPixelsCount (pixelsCount) {
+        return Math.floor(pixelsCount / this.ceil)
+    }
+    public getCeilXPos (index: number): number {
+        return index * this.ceil - (this.ceil / 2)
+    }
 
-                case this.RIGHT:
-                    this.headPosition.x = Phaser.Math.Wrap(this.headPosition.x + 1, 0, 40);
-                    break;
-
-                case this.UP:
-                    this.headPosition.y = Phaser.Math.Wrap(this.headPosition.y - 1, 0, 30);
-                    break;
-
-                case this.DOWN:
-                    this.headPosition.y = Phaser.Math.Wrap(this.headPosition.y + 1, 0, 30);
-                    break;
-            }
-
-            this.direction = this.heading;
-
-            //  Update the body segments
-            Phaser.Actions.ShiftPosition(this.body.getChildren(), this.headPosition.x * 16, this.headPosition.y * 16, 1);
-
-            //  Update the timer ready for the next movement
-            this.moveTime = time + this.speed;
-
-            return true;
+    public getCeilYPos (index: number): number {
+        return index * this.ceil - (this.ceil / 2)
+    }
+    public isAlive (): boolean {
+        return this.alive
+    }
+    public setDead (status: boolean): void {
+        this.alive = status
+    }
+    public getTail () {
+        return this.bodyPartsPositions[0]
+    }
+    
+    public getHead () {
+        return this.bodyPartsPositions[this.getSnakeLength() - 1]
+    }
+    public setDir (wantMoveDir) {
+        const oldDir = this.lastMoveDir
+    
+        if (!oldDir || !wantMoveDir || wantMoveDir === this.getDirOpposite(oldDir)) {
+            return
         }
-
-};
+    
+        this.currentDir = wantMoveDir
+    }
+    public move (): void {
+        const pos = {
+          x: this.snakeHeadX,
+          y: this.snakeHeadY
+        }
+        const dir = this.currentDir
+    
+        // changes pos variable
+        this.changePosByDir(dir, pos)
+        this.snakeHeadX = pos.x
+        this.snakeHeadY = pos.y
+    
+        this.bodyPartsPositions.shift()
+        this.bodyPartsPositions.push({
+          ...pos,
+          dir
+        })
+    
+        // Border Movement
+        this.moveRegardingWorldBoundaries()
+    
+        this.updateBodyPartsPositions()
+        this.updateBodyPartsSpritesFrames()
+        // this.updateBodyPartsBonuses()
+    
+        this.lastMoveDir = this.currentDir
+    }
+    public moveRegardingWorldBoundaries () {
+        const head = this.getHead()
+        const xStartPos = this.getCeilXPos(1)
+        const yStartPos = this.getCeilYPos(1)
+        const xEndPos = this.getCeilXPos(this.ceilsXCount)
+        const yEndPos = this.getCeilYPos(this.ceilsYCount)
+    
+        let x
+        let y
+    
+        if (head.x < xStartPos) {
+          x = xEndPos
+        }
+        if (head.x > xEndPos) {
+          x = xStartPos
+        }
+        if (head.y < yStartPos) {
+          y = yEndPos
+        }
+        if (head.y > yEndPos) {
+          y = yStartPos
+        }
+    
+        this.setSnakeHeadPos(x, y)
+    }
+    public setSnakeHeadPos (x, y) {
+        const head = this.getHead()
+    
+        // x === undefined || x === null
+        if (!x && x !== 0) {
+            x = head.x
+        }
+        if (!y && y !== 0) {
+            y = head.y
+        }
+    
+        head.x = this.snakeHeadX = x
+        head.y = this.snakeHeadY = y
+    }
+    public onTakenBonus (bonus) {
+        // this.growSnake()
+    
+        const posIndex = this.getSnakeLength() - 2 // "- 1" — head
+        const { x, y } = this.bodyPartsPositions[posIndex]
+        const sprite = this.addBodyBonusSprite(x, y)
+    
+        this.bodyPartsBonuses.push({
+            posIndex,
+            sprite
+        })
+    }
+    
+    public growSnake () {
+        const tail = this.getTail()
+        const newDir = this.getDirOpposite(tail.dir)
+        const newTailPos = {
+            ...tail,
+            dir: newDir
+        }
+        this.changePosByDir(newDir, newTailPos)
+    
+        this.bodyPartsPositions.unshift({
+            ...newTailPos
+        })
+        this.bodyPartsSprites.unshift(
+            this.addSprite(newTailPos.x, newTailPos.y)
+        )
+    
+        // end of tail (0 - tail index)
+        this.updateBodyPartsSpritesFrames(0)
+        // and subsequent element
+        this.updateBodyPartsSpritesFrames(1)
+    
+        // as a new element is added to the beginning of the array (end of the tail),
+        // it is necessary to shift the elements by one position so that there is no correspondence
+        this.bodyPartsBonuses = this.bodyPartsBonuses.map(k => ({ ...k, posIndex: ++k.posIndex }))
+    }
+    public getSnakeLength () {
+        return this.bodyPartsPositions.length
+    }
+    private updateBodyPartsPositions () {
+        this.bodyPartsPositions.forEach((pos, i) => {
+            const sprite = this.bodyPartsSprites[i]
+        
+            sprite.x = pos.x
+            sprite.y = pos.y
+        })
+    }
+    private getInitialSnakeBody () {
+        const body: any[] = []
+        const partsCount = this.initialLength
+    
+        for (let i = 0; i < partsCount; i++) {
+            body.unshift({
+                x: this.snakeHeadX,
+                y: this.snakeHeadY + this.snakeLength * i,
+                dir: this.currentDir
+            })
+        }
+    
+        return body
+    }
+    private changePosByDir (dir, pos) {
+        switch (dir) {
+            case this.DIRECTIONS.UP:
+                pos.y -= this.snakeLength
+                break
+            case this.DIRECTIONS.RIGHT:
+                pos.x += this.snakeLength
+                break
+            case this.DIRECTIONS.DOWN:
+                pos.y += this.snakeLength
+                break
+            case this.DIRECTIONS.LEFT:
+                pos.x -= this.snakeLength
+                break
+        }
+    
+        return pos
+    }
+    private buildSnake (): void {
+        for (const bodyPart of this.bodyPartsPositions) {
+            this.bodyPartsSprites.push(
+                this.addSprite(bodyPart.x, bodyPart.y)
+            )
+        }
+    
+        this.updateBodyPartsSpritesFrames()
+    }
+    private addSprite (x, y) {
+        const sprite = this.scene.add.sprite(x, y, 'snake', 4)
+    
+        sprite.setDisplaySize(this.snakeLength, this.snakeLength)
+    
+        return sprite
+    }
+    private updateBodyPartsSpritesFrames (specificPosIndex: number | null = null) {
+        const HEAD = {
+          up: 3,
+          right: 4,
+          down: 9,
+          left: 8
+        }
+        const TAIL = {
+          up: 13,
+          right: 14,
+          down: 19,
+          left: 18
+        }
+        const MIDDLE = {
+          vertical: 7,
+          horizontal: 1
+        }
+    
+        const process = (pos, i) => {
+          const sprite = this.bodyPartsSprites[i]
+          const dir = pos.dir
+          const next = this.bodyPartsPositions[i + 1]
+    
+          const setTex = (frame) => {
+            sprite.setTexture('snake', frame)
+          }
+    
+          // head
+          if (i === this.getSnakeLength() - 1) {
+            setTex(HEAD[dir])
+          } else if (!i) { // tail
+            // tail not turning
+            if (dir === next.dir) {
+              setTex(TAIL[dir])
+            } else { // Tail in the process of turning
+              setTex(TAIL[next.dir])
+            }
+          } else if (dir !== next.dir) { // corners
+            // for example, turnAndReverseTurn('up', 'left', 2) is equivalent to:
+            // "if (dir === 'up' && next.dir === 'left' || dir === 'right' && next.dir === 'down') {"
+            const turnAndReverseTurn = (dirStart, dirTurn, frame) => {
+              if ((dir === dirStart && next.dir === dirTurn)
+                || (dir === this.getDirOpposite(dirTurn) && next.dir === this.getDirOpposite(dirStart))
+              ) {
+                setTex(frame)
+              }
+            }
+    
+            turnAndReverseTurn(this.DIRECTIONS.UP, this.DIRECTIONS.LEFT, 2)
+            turnAndReverseTurn(this.DIRECTIONS.UP, this.DIRECTIONS.RIGHT, 0)
+            turnAndReverseTurn(this.DIRECTIONS.RIGHT, this.DIRECTIONS.UP, 12)
+            turnAndReverseTurn(this.DIRECTIONS.LEFT, this.DIRECTIONS.UP, 5)
+          } else { // middle part
+            // ['right', 'left'].includes(dir)
+            // const frame = ['up', 'down'].includes(dir) ? MIDDLE.vertical : MIDDLE.horizontal
+    
+            setTex([
+                MIDDLE.horizontal,
+                MIDDLE.vertical
+                ][
+                    +[this.DIRECTIONS.UP, this.DIRECTIONS.DOWN].includes(dir)
+                ])
+            }
+        }
+    
+        if (specificPosIndex !== null) {
+            process(
+                this.bodyPartsPositions[specificPosIndex],
+                specificPosIndex
+            )
+        } else {
+            this.bodyPartsPositions.forEach(process)
+        }
+    }
+    private getDirOpposite (dir) {
+        const dirs = [
+            [this.DIRECTIONS.UP, this.DIRECTIONS.DOWN],
+            [this.DIRECTIONS.RIGHT, this.DIRECTIONS.LEFT]
+        ]
+        
+        for (const dirArr of dirs) {
+            if (dirArr.includes(dir)) {
+                return dirArr.find((i: string) => i !== dir)
+            }
+        }
+    }
+    private addBodyBonusSprite (x, y) {
+        const sprite = this.scene.add.sprite(x, y, 'snake', 16) // 15
+    
+        sprite.setDepth(3)
+        sprite.setDisplaySize(this.snakeLength / 2, this.snakeLength / 2)
+    
+        return sprite
+    }
+}
--- src/game.ts
+++ src/game.ts
@@ -5,7 +5,10 @@ export default class Demo extends Phaser.Scene
 {
     private snake!: Snake;
     private worldIterations = 0
-    private cursors;
+    private bonusesPositions: any[] = []
+    private penaltyPositions: any[] = []
+    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
+    
     
     //  Direction consts
     
@@ -32,6 +35,10 @@ export default class Demo extends Phaser.Scene
             .setCircle(8).setScale(4);
 
         this.snake = new Snake(this, -1, 0);
+
+        // input
+        this.cursors = this.input.keyboard.createCursorKeys()
+        this.addEventListeners()
         
     }
     update (time, delta)
@@ -42,20 +49,121 @@ export default class Demo extends Phaser.Scene
         this.worldIterations += 1
     
         if (this.worldIterations % snakeMoveIterationsRange === 0) {
-        //   this.handleInput()
-          this.snake.move()
+            this.handleInput()
+            this.snake.move()
     
-        //   this.checkCollision()
+            this.checkCollision()
         }
     
         // this.fpsText.setText(this.getFPS())
     
-        // if (!this.snake.alive()) {
-        //   this.onDead()
-        // pause game
-        // this.scene.pause();
+        if (!this.snake.isAlive()) {
+            this.onDead()
+            // pause game
+            // this.scene.pause();
+        }
+    }
+    public handleInput (): void {
+        const wantMoveDir = this.getDirByInput()
+    
+        this.snake.setDir(wantMoveDir)
+    }
+    
+    public getDirByInput (presedKey?) {
+        const input = {
+          // Format: [button]: key
+            [this.snake.DIRECTIONS.UP]: ['w', 'ArrowUp'],
+            [this.snake.DIRECTIONS.RIGHT]: ['d', 'ArrowRight'],
+            [this.snake.DIRECTIONS.DOWN]: ['s', 'ArrowDown'],
+            [this.snake.DIRECTIONS.LEFT]: ['a', 'ArrowLeft']
+        }
+    
+        // Same as below:
+        for (const [button, keys] of Object.entries(input)) {
+            if ((this.cursors[button] && this.cursors[button].isDown) || keys.includes(presedKey)) {
+                return button
+            }
+        }
+        // Same as above:
+        // switch (true) {
+        //   case (this.cursors.up && this.cursors.up.isDown) || presedKey === 'w':
+        //     return this.snake.DIRECTIONS.UP
+        //   case (this.cursors.right && this.cursors.right.isDown) || presedKey === 'd':
+        //     return this.snake.DIRECTIONS.RIGHT
+        //   case (this.cursors.down && this.cursors.down.isDown) || presedKey === 's':
+        //     return this.snake.DIRECTIONS.DOWN
+        //   case (this.cursors.left && this.cursors.left.isDown) || presedKey === 'a':
+        //     return this.snake.DIRECTIONS.LEFT
         // }
-      }
+    }
+    private checkTaran () {
+        const taran = this.snake.bodyPartsPositions
+            .slice(0, -1)
+            .find(i =>
+                i.x === this.snake.snakeHeadX && i.y === this.snake.snakeHeadY
+            )
+    
+        this.snake.setDead(taran)
+    }
+    
+    private checkApplyBonus () {
+        const snakeHalfSize = this.snake.snakeLength / 2
+    
+        const bonusPos = this.bonusesPositions.find(({ x: bonusX, y: bonusY }) =>
+            this.snake.bodyPartsPositions.some(({ x: bodyX, y: bodyY }: any) =>
+                bonusX >= bodyX - snakeHalfSize &&
+                bonusY >= bodyY - snakeHalfSize &&
+                bonusX <= bodyX + snakeHalfSize &&
+                bonusY <= bodyY + snakeHalfSize
+            )
+        )
+    
+        if (bonusPos) {
+            const bonus = bonusPos.bonus
+            const randomSoundKey = 1 + Math.round(Math.random() * (5 - 1))
+        
+            this.bonusesPositions = this.bonusesPositions.filter(i => i !== bonusPos)
+        
+            bonus.onCollisionWithSnake()
+            this.snake.onTakenBonus(bonus)
+            // this.soundManager.play('eat' + randomSoundKey)
+        }
+    }
+    private checkCollision (): void {
+        this.checkApplyBonus()
+        this.checkTaran()
+    }
+    private addEventListeners () {
+        this.input.keyboard.on('keydown', ({ key }) => {
+            this.snake.setDir(
+                this.getDirByInput(key)
+            )
+        })
+        // this.events.on('pause', () => {
+        //     this.timer.pause()
+        // })
+        // this.events.on('resume', () => {
+        //     this.timer.resume()
+        // })
+        // mobile controls
+        // window.game.events.on('swipe', (dir) => {
+        //     this.snake.setDir(dir)
+        // })
+        // window.game.events.on('app_toggle_debug', (isDebug: boolean) => {
+        //     this.fpsText.setVisible(isDebug)
+        // })
+    }
+    private onDead () {
+        // const score = this.overlay.getApplesCounter()
+        // const maxScore = this.storage.get('max-score')
+    
+        // if (score > maxScore || maxScore === undefined) {
+        //   this.storage.set('max-score', score)
+        // }
+    
+        // this.soundManager.play('dead')
+        // this.scene.start('MainMenuScene')
+    }
 }
 
 const config = {
